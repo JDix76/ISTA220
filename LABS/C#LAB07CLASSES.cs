575699ab11dc4ca30135cc0747b0d6c181b62474
James Dix <42378279+JDix76@users.noreply.github.com>
2018-08-30 15:14:05 -0400

    Add files via upload

---
+++ LABS/C#LAB07CLASSES.cs
@@ -0,0 +1,41 @@
+﻿--Author: James Dix
+--Date: 29 August 2018
+--Subject: C#LAB07
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Point origin = new Point();
+            Point bottomRight = new Point(1366, 768);
+            Point ajay = new Point(77, 12);
+            double distance = origin.DistanceTo(bottomRight);
+            double distance2 = ajay.DistanceTo(origin);
+
+            int oc = Point.ObjectCount();
+            Console.WriteLine($"The object count is {oc}");
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ LABS/C#LAB08PARAMETERS.cs
@@ -0,0 +1,76 @@
+﻿--AUTHOR: JAMES DIX
+--DATE: 30 AUG 2018
+--SUBJECT: C#LAB08
+#region using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Parameters
+{
+    class Program
+    {
+        static void doWork()
+        {
+            int i = 0;
+            Console.WriteLine(i);
+            Pass.Value(i);
+            Console.WriteLine(i);
+
+            WrappedInt wi = new WrappedInt();
+            Console.WriteLine(wi.Number);
+            Pass.Reference(wi);
+            Console.WriteLine(wi.Number);
+
+            int j = 0;
+            Console.WriteLine(j);
+            Pass.Value1(ref j);
+            Console.WriteLine(j);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
+using System;
+
+namespace Parameters
+{
+    class Pass
+    {
+        public static void Value(ref int param)
+        {
+            param = 42;
+        }
+
+        public static void Reference(WrappedInt param)
+        {
+            param.Number = 42;
+        }
+    }
+}
+
+namespace Parameters
+{
+    class WrappedInt
+    {
+        public int Number;
+    }
+}
+
+
+
+

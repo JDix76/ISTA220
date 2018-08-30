991d5b79b92645ac60d89b5f7d871c8821d6e8da
James Dix <42378279+JDix76@users.noreply.github.com>
2018-08-30 15:12:11 -0400

    Add files via upload

---
+++ EXERCISES/C#EXE2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace sum2ten
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char grade = 'z';
+
+
+            //Ask the user for score input
+            Console.WriteLine("Please input your score:");
+
+
+            //Assign a letter grade to the average of the score
+            int userinput = Convert.ToInt32(Console.ReadLine());
+            if (userinput > 90)
+
+            {
+                grade = 'A';
+            }
+
+            else if (userinput >= 80 && userinput < 90)
+            {
+                grade = 'B';
+            }
+
+            else if (userinput >= 70 && userinput < 80)
+            {
+                grade = 'C';
+            }
+
+            else if (userinput >= 60 && userinput < 70)
+            {
+                grade = 'D';
+            }
+
+            else
+            {
+                grade = 'F';
+            }
+            Console.WriteLine($"Your grade is:{grade}");
+
+
+
+
+
+
+        }
+
+    }
+}
+
+       
+
+
+
+
+
+
---
+++ EXERCISES/C#EXE2Q2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+
+            int start = 0;
+            int end = 10;
+            int sum = 0;
+            get_sum(start, end, sum);
+            int scoreval = Convert.ToInt32(Console.ReadLine());
+        }
+
+        private static void get_sum(int start, int end, int sum)
+        {
+
+            start = start + 1;
+            if (start == 1)
+            {
+                //Tell user to input the scores
+                Console.WriteLine($"Please type in 10 test scores");
+                
+            }
+
+            int userinput = Convert.ToInt32(Console.ReadLine());
+
+
+            sum = sum + userinput;
+            if (start < end)
+                get_sum(start, end, sum);
+            else Console.WriteLine($"The average of the all 10 test scores is: {sum/10}");
+
+
+        }
+           
+    }
+
+        
+
+
+
+
+}

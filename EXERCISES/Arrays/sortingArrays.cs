4186fded387a293cf3f55397d434656b75ebcdd6
James Dix <42378279+JDix76@users.noreply.github.com>
2018-09-10 00:32:01 -0400

    Add files via upload

---
+++ EXERCISES/Arrays/sortingArrays.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] C = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9 };
+            int temp = 0;
+
+            for (int i = 0; i < C.Length; i++)
+            {
+                for (int j = 0; j < C.Length; j++)
+                {
+                    if (C[i] < C[j])
+                    {
+                        temp = C[i];
+
+                        C[i] = C[j];
+
+                        C[j] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < C.Length; i++)
+            {
+                Console.Write("{0}, ", C[i]);
+            }
+        }
+    }
+}
+
+
+
+

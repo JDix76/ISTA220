748a482af83cfbae1fb512b577514c0f517340aa
James Dix <42378279+JDix76@users.noreply.github.com>
2018-09-09 23:38:29 -0400

    Add files via upload

---
+++ EXERCISES/Arrays/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 0, 2, 4, 6, 8, 10 };
+            int[] B = { 1, 3, 5, 7, 9 };
+            int[] C = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9 };
+
+            int count = 0;
+            int sum = 0;
+            double mean = 0.0;
+
+            foreach (int e in A)
+            {
+                count++;
+                sum = e + sum;
+                mean = (double)sum / count;
+            }
+            //foreach (int d in B)
+            //{
+            //    count++;
+            //    sum = d + sum;
+            //    mean = (double)sum / count;
+            //}
+            //foreach (int f in C)
+            //{
+            //    count++;
+            //    sum = f + sum;
+            //    mean = (double)sum / count;
+            //}
+
+
+            Console.WriteLine($"The count for Array A is: {count}");
+            Console.WriteLine($"The sum for Array A is:  {sum}");
+            Console.WriteLine($"The mean for Array A is {mean}");
+            Console.WriteLine();
+
+            //Console.WriteLine($"The count for Array B is: {count}");
+            //Console.WriteLine($"The sum for Array B is:  {sum}");
+            //Console.WriteLine($"The mean for Array B is {mean}");
+            //Console.WriteLine();
+
+            //Console.WriteLine($"The count for Array C is: {count}");
+            //Console.WriteLine($"The sum for Array C is:  {sum}");
+            //Console.WriteLine($"The mean for Array C is {mean}");
+            //Console.WriteLine();
+
+
+            Console.WriteLine("Array A is: ");
+            Console.WriteLine(string.Join(" , ", A));
+            Array.Reverse(A);
+            Console.WriteLine("The Reverse of Array A : ");
+            Console.WriteLine(string.Join(" , ", A));
+            Console.WriteLine();
+
+            Console.WriteLine("Array B is:");
+            Console.WriteLine(string.Join(" , ", B));
+            Array.Reverse(B);
+            Console.WriteLine("The Reverse of Array B: ");
+            Console.WriteLine(string.Join(" , ", B));
+            Console.WriteLine();
+
+            Console.WriteLine("Array C is:");
+            Console.WriteLine(string.Join(" , ", C));
+            Array.Reverse(C);
+            Console.WriteLine("The Reverse of Array C: ");
+            Console.WriteLine(string.Join(" , ", C));
+            Console.WriteLine();
+
+            
+
+            
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ EXERCISES/Arrays/rotatingArrays.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rotate_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+      
+        {
+                //int[] A = { 0, 2, 4, 6, 8, 10 };
+                int[] B = { 1, 3, 5, 7, 9 };
+                //int[] C = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9 };
+                LeftRotation(B, B.Length, 2);
+                Print(B, B.Length);
+                Console.ReadLine();
+        }
+
+        private static void sort(int[] arr)
+        {
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int k = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = k;                        
+                    }
+                }
+                Console.Write($" {arr[i]}");                
+            }
+            Console.WriteLine("");
+        }
+
+        private static void Print(int[] b, int n)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(b[i] + " , ");
+                }
+        }
+
+         private static void LeftRotation(int[] b, int n, int k)
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    int temp = b[0];
+                    for (int j = 1; j < n; j++)
+                    {
+                        b[j - 1] = b[j];
+                    }
+
+                    b[n - 1] = temp;
+                }
+
+         }
+
+        
+    }
+
+
+}
+    
+
